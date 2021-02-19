030810c57da6cb791657472c0ec5fe11bc5bda5d
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-02-19 22:16:11 +1100

    create constructor lib & prompt foundation

---
+++ index.js
@@ -0,0 +1,132 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./lib/employee');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+const valCheck = (value) => { if(value){return true} else {return 'Please enter a value.'}};
+const teamMembers = []
+
+function addMember() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+            default: 'Jim Bob',
+            validate: valCheck 
+        },
+
+        {
+            type: 'list',
+            message: 'What is your role?',
+            name: 'role',
+            choices: [
+                'Manager', 
+                'Engineer', 
+                'Intern', 
+            ],
+            validate: valCheck
+        },
+
+        {
+            type: 'input',
+            message: 'Please enter your ID#.',
+            name: 'id',
+            validate: valCheck
+        },
+
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+            default: 'jimbob@fakemail.com',
+            validate: valCheck
+        }
+    ])
+    .then(function({name, role, id, email}) {
+        if (role === 'Manager') {
+            console.log('Mgr');
+        }
+        else if (role === 'Engineer') {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: 'What is your GitHub username? (w/o @)',
+                    name: 'git',
+                    default: 'jb-Git',
+                    validate: valCheck
+                }              
+            ])
+            .then(function({git}) {
+                let newMember = new Engineer(name, id, email, git);
+                teamMembers.push(newMember);
+                console.log(teamMembers);
+                // add to HTML also
+
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        message: 'Add another team member?',
+                        name: 'addMore',
+                        choices: [
+                            'Yes', 
+                            'No'
+                        ],
+                    }
+                ])
+                .then(function({addMore}) {
+                    if (addMore === 'Yes') {
+                        addMember();
+                    }
+                    else if (addMore === 'No') {
+                        // create end fxn
+                        exitAdd();
+                    }
+                })                
+            })
+
+        }
+        else if (role === 'Intern') {
+            console.log('Noob');
+            // inquirer.prompt([
+            //     {
+            //         type: 'input',
+            //         message: 'Which school did you attend?',
+            //         name: 'school',
+            //         default: 'noobVersity',
+            //         validate: valCheck
+            //     },
+            // ])
+        }
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
+}
+
+function exitAdd() {
+    console.log('Thank you for using the Team Generator!')
+}
+
+addMember();
+
+// function writeToFile(path, data) {
+//     fs.writeFile(path, data, err => {
+//         if(err){
+//             console.log(err)
+//         }
+//         else{console.log('Your README has generated successfully!')}
+//     })
+// }
+
+// async function init() {
+//     await inquirer
+//     .prompt(questions)
+//     .then(response => {
+//         writeToFile(path, generateMarkdown(response));
+//         console.log('Thank you for using the MissNG README generator! :)');
+//     });    
+// }
+
+// init();
